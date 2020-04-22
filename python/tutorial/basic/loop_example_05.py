6786562d2d1d2340304b1257cd727cbc2653dcde
Victor Cleber <victor.cleber@outlook.com>
2020-04-22 13:32:24 +1200

    Moving python folder

---
+++ python/tutorial/basic/loop_example_05.py
@@ -0,0 +1,10 @@
+print("Example 05: [The else Statement]  \n"
+      "            Print a message once the condition is false")
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("The condition is false")
+
