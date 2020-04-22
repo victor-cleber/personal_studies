b1eac6edf557fff8167fb45210b82a84839f71cd
Victor Cleber <victor.cleber@outlook.com>
2020-04-22 13:29:34 +1200

    Moving python folder

--- python/tutorial/basic/loop_example_03.py
+++
@@ -1,9 +0,0 @@
-print("Example 03: [The break statement] \n"
-      "            Exit the loop when i is 5")
-
-i = 1
-while i < 10:
-  print(i)
-  if i == 5:
-    break
-  i += 1
--- python/tutorial/basic/loop_example_04.py
+++
@@ -1,9 +0,0 @@
-print("Example 04: [The continue Statement] \n"
-      "            Continue to the next iteration if i is 5")
-
-i = 0
-while i < 10:
-  i += 1
-  if i == 5:
-    continue
-  print(i)
